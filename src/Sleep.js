809c11c30009f3cff0b950cadaa5c5144f4ba916
Robert Aiken <robertsiraaiken@gmail.com>
2022-12-07 17:31:20 -0600

    Add getWeeklyHoursSlept and getWeeklySleepQuality methods

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,5 +1,5 @@
 class Sleep {
-  constructor(currentUser, data){
+  constructor(currentUser, data) {
     this.userSleepInfo = data.sleepData.filter(
       (day) => day.userID === currentUser.id
     );
@@ -7,8 +7,26 @@ class Sleep {
     this.sleepData = data.sleepData;
   }
 
-  averageSleepQuality(){
-    return (this.userSleepInfo.map(day => day.sleepQuality).reduce((a, b) => a + b) / this.userSleepInfo.length).toFixed(1)
+  averageSleepQuality() {
+    return (
+      this.userSleepInfo
+        .map((day) => day.sleepQuality)
+        .reduce((a, b) => a + b) / this.userSleepInfo.length
+    ).toFixed(1);
+  }
+
+  getWeeklyHoursSlept(date) {
+    let index = this.userSleepInfo.findIndex((day) => day.date === date);
+    return this.userSleepInfo
+      .slice(index - 6, index + 1)
+      .map((day) => (day = { date: day.date, hoursSlept: day.hoursSlept }));
+  }
+
+  getWeeklySleepQuality(date) {
+    let index = this.userSleepInfo.findIndex((day) => day.date === date);
+    return this.userSleepInfo
+      .slice(index - 6, index + 1)
+      .map((day) => (day = { date: day.date, sleepQuality: day.sleepQuality }));
   }
 }
 
