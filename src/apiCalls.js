001c01ec48faae3e81343ef11be29301c6742180
Robert Aiken <robertsiraaiken@gmail.com>
2022-12-09 15:30:20 -0600

    Add fetchData function and refactoring

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,13 @@
 // Your fetch requests will live here!
 
+function fetchData(dataset) {
+    return fetch(`https://fitlit-api.herokuapp.com/api/v1/${dataset}`).then(response => response.json()).catch(error => console.log(`${dataset}`, error))
+}
+
 export default function returnDataPromises(){
-    const fetchUsers = fetch('https://fitlit-api.herokuapp.com/api/v1/users').then(response => response.json()).catch(error => console.log('Users: ', error))
-    const fetchSleep = fetch('https://fitlit-api.herokuapp.com/api/v1/sleep').then(response => response.json()).catch(error => console.log('Sleep: ', error))
-    const fetchHydration = fetch('https://fitlit-api.herokuapp.com/api/v1/hydration').then(response => response.json()).catch(error => console.log('Hydration: ', error))
+    const fetchUsers = fetchData("users")
+    const fetchSleep = fetchData("sleep")
+    const fetchHydration = fetchData("hydration")
 
     return Promise.all([fetchUsers, fetchSleep, fetchHydration])
 }
