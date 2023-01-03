71f6c66c5e44440531574420e38f685e8e35ad35
Max Lange <max@langes.us>
2023-01-03 17:28:50 -0500

    Add method to calculate miles walked per a given day

--- src/Activity.js
+++ src/Activity.js
@@ -3,6 +3,14 @@ class Activity {
     this.userActivityInfo = data.activityData.filter(
       (day) => day.userID === currentUser.id
     );
+    this.currentUser = currentUser;
+  }
+  
+  givenDayMilesWalked(givenDate) {
+    const currentStride = this.currentUser.strideLength;
+    return ((this.userActivityInfo.find(day => {
+      return day.date === givenDate
+    }).map(day => day.numSteps) * currentStride) / 5280).toFixed(1)
   }
 }
 
