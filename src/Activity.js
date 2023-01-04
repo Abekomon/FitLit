865747b8d0198b11ac63a42c0253ba297300af65
Robert Aiken <robertsiraaiken@gmail.com>
2023-01-03 19:35:33 -0600

    Add dailyStepGoalHit method to Activity class

--- src/Activity.js
+++ src/Activity.js
@@ -28,6 +28,10 @@ class Activity {
       }, 0) / 7).toFixed(1))
   }
 
+  dailyStepGoalHit(date){
+    return this.currentUser.dailyStepGoal <= this.userActivityInfo.find(day => day.date === date).numSteps
+  }
+
   stairClimbRecord() {
     let sorted = this.userActivityInfo.sort((a,b) => {return b.flightsOfStairs - a.flightsOfStairs})
     return sorted[0].flightsOfStairs
